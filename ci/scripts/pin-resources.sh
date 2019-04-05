2196cf03e14bf9765704db1a0e90a942e9ec2ea7
Joe Goller <joe.goller@aon.com>
2019-04-05 09:00:40 -0700

    Pin resources task checks for available versions and updates before pinning
    Signed-off-by: Alan Moran <bonzofenix@gmail.com>

--- ci/scripts/pin-resources.sh
+++ ci/scripts/pin-resources.sh
@@ -18,6 +18,8 @@ function pin_versions(){
 
     version_id=$(echo $versions_response | jq -r ".[] | select(.version.version | contains(\"$version_regex\")) | .id")
 
+    fly -t concourse check-resource -r $PIPELINE_NAME/$resource_name -f version:$version_regex
+    sleep 1
     fly -t concourse curl /api/v1/teams/$CONCOURSE_TEAM/pipelines/$PIPELINE_NAME/resources/$resource_name/versions/$version_id/pin -- -k -X PUT
   done < $PINS_FILE
 }
