b09e232cca78908584e01c22ca8e175b0909c78f
Alan Moran <bonzofenix@gmail.com>
2019-06-26 19:39:05 +0000

    Refactor bosh tasks
    Signed-off-by: Joe Goller <joe.goller@aon.com>

--- ci/scripts/bosh/update_cloud_config.sh
+++ ci/scripts/bosh/update_cloud_config.sh
@@ -5,22 +5,11 @@ set -e
 
 source pipeline/ci/scripts/common.sh
 
-function authenticate_director() {
-
-  export BOSH_CLIENT=$( bosh int $OUTPUT/creds.yml --path /bosh/client )
-  export BOSH_CLIENT_SECRET=$( bosh int $OUTPUT/creds.yml --path /bosh/client_secret )
-  export BOSH_ENVIRONMENT=$( bosh int $OUTPUT/creds.yml --path /bosh/environment)
-
-  export BOSH_CA_CERT=$OUTPUT/bosh.crt
-  bosh int $OUTPUT/creds.yml --path /bosh/ca_cert > $BOSH_CA_CERT
-}
-
 function update_cloud_config() {
   bosh -n update-cloud-config $OUTPUT/cloud-config.yml \
     --vars-file=$OUTPUT/vars.yml \
     -v vm_disk_type=thick
 }
-
 
 generate_configs
 authenticate_director
--- ci/scripts/bosh/update_runtime_config.sh
+++ ci/scripts/bosh/update_runtime_config.sh
@@ -5,19 +5,9 @@ set -e
 
 source pipeline/ci/scripts/common.sh
 
-function authenticate_director() {
-  export BOSH_CLIENT=$( bosh int $OUTPUT/creds.yml --path /bosh/client )
-  export BOSH_CLIENT_SECRET=$( bosh int $OUTPUT/creds.yml --path /bosh/client_secret )
-  export BOSH_ENVIRONMENT=$( bosh int $OUTPUT/creds.yml --path /bosh/environment)
-
-  export BOSH_CA_CERT=$OUTPUT/bosh.crt
-  bosh int $OUTPUT/creds.yml --path /bosh/ca_cert > $BOSH_CA_CERT
-}
-
 function update_runtime_config() {
   bosh -n update-runtime-config $OUTPUT/runtime-config.yml
 }
-
 
 generate_configs
 authenticate_director
