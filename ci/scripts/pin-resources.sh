8f4c229bed8d6fbd00fc31445941ad8f74bc49f5
Alan Moran <bonzofenix@gmail.com>
2019-06-27 14:36:33 +0000

    Waits for 20s for check-resource to finish before trying to pin a version
    Signed-off-by: Anand Gopal <anandkumar.gopalkrishnan@aon.com>

--- ci/scripts/pin-resources.sh
+++ ci/scripts/pin-resources.sh
@@ -16,10 +16,11 @@ function pin_versions(){
 
     versions_response="$(fly -t concourse curl /api/v1/teams/$CONCOURSE_TEAM/pipelines/$PIPELINE_NAME/resources/$resource_name/versions -- -k)"
 
+    log "Pinning $resource_name with version matching: $version_regex"
     version_id=$(echo $versions_response | jq -r ".[] | select(.version.version | contains(\"$version_regex\")) | .id")
 
     fly -t concourse check-resource -r $PIPELINE_NAME/$resource_name -f version:$version_regex
-    sleep 10
+    sleep 20
     fly -t concourse curl /api/v1/teams/$CONCOURSE_TEAM/pipelines/$PIPELINE_NAME/resources/$resource_name/unpin -- -k -X PUT
     fly -t concourse curl /api/v1/teams/$CONCOURSE_TEAM/pipelines/$PIPELINE_NAME/resources/$resource_name/versions/$version_id/pin -- -k -X PUT
   done < $PINS_FILE
