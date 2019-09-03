60d045808b99986058902d25aa1da7f916485622
Joe Goller <joe.goller@aon.com>
2019-09-03 15:45:36 +0000

    Updates store file when running update runtime config cmds
    Signed-off-by: Alan Moran <bonzofenix@gmail.com>

--- ci/scripts/bosh/update_runtime_config.sh
+++ ci/scripts/bosh/update_runtime_config.sh
@@ -6,7 +6,9 @@ set -e
 source pipeline/ci/scripts/common.sh
 
 function update_runtime_config() {
-  bosh -n update-runtime-config $OUTPUT/runtime-config.yml
+  bosh -n update-runtime-config \
+    --vars-store $OUTPUT/store.yml \
+    $OUTPUT/runtime-config.yml
 }
 
 trap "sanitize_store && commit_config" EXIT
