f4354878ed886b16d6dbc999d7c0e274815e441e
Joe Goller <joe.goller@aon.com>
2020-01-07 17:07:13 +0000

    Interpolates runtime config when updating

--- ci/scripts/bosh/update_runtime_config.sh
+++ ci/scripts/bosh/update_runtime_config.sh
@@ -6,7 +6,15 @@ set -e
 source pipeline/ci/scripts/common.sh
 
 function update_runtime_config() {
+  local bosh_args=""
+
   generate_releases_version_file
+
+  bosh -n interpolate \
+    --vars-store $OUTPUT/store.yml \
+    --vars-file $OUTPUT/vars.yml \
+    -l $OUTPUT/releases_versions.yml \
+    $OUTPUT/runtime-config.yml
 
   bosh -n update-runtime-config \
     --vars-store $OUTPUT/store.yml \
