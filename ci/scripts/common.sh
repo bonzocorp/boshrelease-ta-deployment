695eff48dea1831ff03fb55e50f69e6edc4b4eda
Alan Moran <alan.moran1@aon.com>
2020-03-05 14:35:09 +0000

    Update custom ca cert managment
    Signed-off-by: Adam Roffler <adam.roffler@aon.com>

--- ci/scripts/common.sh
+++ ci/scripts/common.sh
@@ -13,12 +13,9 @@ OUTPUT=output
 mkdir -p $OUTPUT
 
 function load_custom_ca_certs(){
-  if [[ ! -z "$CUSTOM_ROOT_CA" ]] ; then
-    echo -e "$CUSTOM_ROOT_CA" > /etc/ssl/certs/custom_root_ca.crt
-  fi
-
-  if [[ ! -z "$CUSTOM_INTERMEDIATE_CA" ]] ; then
-    echo -e "$CUSTOM_INTERMEDIATE_CA" > /etc/ssl/certs/custom_intermediate_ca.crt
+  if [[ ! -z "$CUSTOM_CA_CERTS" ]] ; then
+    echo -e "$CUSTOM_CA_CERTS" > custom_certs.crt
+    csplit -k -f /etc/ssl/certs/ custom_certs.crt '/END CERTIFICATE/+1' {10}
   fi
 
   update-ca-certificates
@@ -51,11 +48,19 @@ function log() {
   echo -e "${green}$1${reset}"
 }
 
+function warning() {
+  orange='\033[1;33m'
+  reset='\033[0m'
+
+  echo -e "${orange}$1${reset}"
+}
+
 function error() {
   red='\033[0;31m'
   reset='\033[0m'
 
   echo -e "${red}$1${reset}"
+  exit 1
 }
 
 function check_if_exists(){
